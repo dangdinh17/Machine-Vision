02b042f5db9dfd2cee594ec21c91dbd8b1344be8
SangNQ <ngsang998@gmail.com>
2025-12-24 00:27:42 +0800

    add

--- train_e2e_deep.py
+++ train_e2e_deep.py
@@ -30,7 +30,7 @@ from torch.cuda.amp import autocast, GradScaler
 
 def receive_arg():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--opt_path', type=str, default='configs/train_e2e_neu.yml', help='Path to option YAML file.')
+    parser.add_argument('--opt_path', type=str, default='configs/train_e2e_deep.yml', help='Path to option YAML file.')
     parser.add_argument('--local_rank', type=int, default=0, help='Distributed launcher requires.')
     # parser.add_argument('--imgsz', type=int, default=64, help='Image size for training.')
     # parser.add_argument('--exp_name', type=str, default='Enhancer_LR_64', help='Experiment name for logging.')
